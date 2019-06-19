03a65fa1874e4bd0a321291ed7d867893d99dc50
John Ghioca <John.Ghioca@akqa.com>
2019-06-19 17:11:40 -0400

    making h1 copy dynamic

--- onboarding/src/components/atoms/H1/index.js
+++ onboarding/src/components/atoms/H1/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './H1.module.css';
 
-const H1 = props => {
-  return <h1 className={styles.h1}>H1</h1>;
+const H1 = ({ children }) => {
+  return <h1 className={styles.h1}>{children}</h1>;
 };
 
 H1.propTypes = {

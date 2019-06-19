d3c10f1625e2924e5b6c41aa1c36810d2fc7551f
John Ghioca <John.Ghioca@akqa.com>
2019-06-19 13:17:56 -0400

    eyebrow component created

---
+++ onboarding/src/components/atoms/Eyebrow/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Eyebrow.module.css';
+
+const Eyebrow = ({ children }) => {
+  return <p class={styles.eyebrow}>{children}</p>;
+};
+
+Eyebrow.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Eyebrow;

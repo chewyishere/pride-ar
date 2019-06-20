12d09fc600e3e9033f1ae47f5767e0f944a17083
John Ghioca <John.Ghioca@akqa.com>
2019-06-20 16:15:21 -0400

    added Header

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'App.css';
-import { ReactComponent as Logo } from 'assets/img/pride-march-logo-white.svg';
 import Eyebrow from 'components/atoms/Eyebrow';
 import H1 from 'components/atoms/H1';
 import H2 from 'components/atoms/H2';
@@ -10,15 +9,15 @@ import Section from 'components/atoms/Section';
 import Button from 'components/atoms/Button';
 import ButtonRefresh from 'components/atoms/ButtonRefresh';
 import Number from 'components/atoms/Number';
+import Header from 'components/molecules/Header';
 
 function App() {
   const colors = getComputedStyle(document.documentElement);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo />
-        {/* <a
+    <div className="home">
+      <Header text="1969-2019" />
+      {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -26,40 +25,39 @@ function App() {
         >
           Learn React
         </a> */}
-        <Eyebrow>Eyebrow</Eyebrow>
-        <H1>H1</H1>
-        <H2>H2</H2>
-        <Year>1969-2019</Year>
-        <Section>
-          <P>
-            Fifty years ago, the Stonewall Uprising sparked the modern day
-            LGBTQ+ liberation movement — creating a blaze of activism and
-            awareness. To shine a light on the milestones and sacrifices that
-            brought us to this moment, we created a way for the community and
-            its allies to bring the story of NYC Pride to life.
-          </P>
+      <Eyebrow>Eyebrow</Eyebrow>
+      <H1>H1</H1>
+      <H2>H2</H2>
+      <Year>1969-2019</Year>
+      <Section>
+        <P>
+          Fifty years ago, the Stonewall Uprising sparked the modern day LGBTQ+
+          liberation movement — creating a blaze of activism and awareness. To
+          shine a light on the milestones and sacrifices that brought us to this
+          moment, we created a way for the community and its allies to bring the
+          story of NYC Pride to life.
+        </P>
 
-          <P>
-            Please enjoy our immersive AR experience, narrated by Gerald Busby
-            who witnessed the movement first hand, and rally with our cast of
-            colorful Icons — each representing pivotal moments from the past. So
-            show some love, join the march and let the world know, the only
-            direction we’re moving is forward.
-          </P>
+        <P>
+          Please enjoy our immersive AR experience, narrated by Gerald Busby who
+          witnessed the movement first hand, and rally with our cast of colorful
+          Icons — each representing pivotal moments from the past. So show some
+          love, join the march and let the world know, the only direction we’re
+          moving is forward.
+        </P>
 
-          <Button
-            href="/test"
-            bgColor={colors.getPropertyValue('--pale-red')}
-            color={colors.getPropertyValue('--silver')}
-          >
-            Let's go
-          </Button>
+        <Button
+          href="/test"
+          bgColor={colors.getPropertyValue('--pale-red')}
+          color={colors.getPropertyValue('--silver')}
+        >
+          Let's go
+        </Button>
 
-          <ButtonRefresh>Test</ButtonRefresh>
+        <ButtonRefresh>Test</ButtonRefresh>
 
-          <Number>{3}</Number>
-        </Section>
-      </header>
+        <Number>{3}</Number>
+      </Section>
     </div>
   );
 }
